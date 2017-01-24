76b27b5033691204caac952123c8dcac166afd73
David Laundav <dla@247e.com>
2017-01-25 00:21:43 +0100

    Refactoring

--- Test/MainActivity.cs
+++ Test/MainActivity.cs
@@ -1,29 +1,30 @@
 ﻿using Android.App;
-using Android.Widget;
+using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Widget;
 
-namespace Test
+namespace VectorDrawables
 {
-    [Activity(Label = "Test", MainLauncher = true, Icon = "@mipmap/icon")]
+    [Activity(Label = "VectorDrawables", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : AppCompatActivity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            AppCompatDelegate.CompatVectorFromResourcesEnabled = true;
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate 
+            {
+                StartActivity(new Intent(this, typeof(Second)));
+            };
+
+            //var image = FindViewById<ImageView>(Resource.Id.imageView);
+
+            //image.SetImageDrawable(ContextCompat.GetDrawable(this, Resource.Drawable.rotate));
         }
     }
 }
---
+++ Test/Second.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace VectorDrawables
+{
+    [Activity(Label = "VectorDrawables")]
+    public class Second : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            AppCompatDelegate.CompatVectorFromResourcesEnabled = true;
+
+            SetContentView(Resource.Layout.Second);
+        }
+    }
+}
